32f5ade3ccae9840f9117dd176f5bc8a94bcad8d
zaidunz <zid4nsyhid@gmail.com>
2024-09-24 12:22:50 +0700

    memprogram Hello World pada Jurnal1

--- Jurnal1.java
+++ Jurnal1.java
@@ -2,5 +2,7 @@ public class Jurnal1 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        String greString="Hello World";
+        System.out.println(greString);
     }
 }
