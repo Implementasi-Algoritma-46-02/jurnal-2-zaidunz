96ef304c1010ec0656a66747681f1e8d29e35092
zaidunz <zid4nsyhid@gmail.com>
2024-09-24 17:24:59 +0700

    menambahkan hello world!!

--- Jurnal1.java
+++ Jurnal1.java
@@ -2,7 +2,6 @@ public class Jurnal1 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
-        String greString="Hello World";
-        System.out.println(greString);
+        System.out.println("Hello World!!");
     }
 }
--- zidan.java
+++
@@ -1,6 +0,0 @@
-public class zidan {
-    public static void main(String[] args) {
-        System.out.println("Zidan");
-    }
-    
-}
